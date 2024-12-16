a394fa970f37e4bb5130ec272ef46fc5161dfc1a
Ajit Gadkari <ajitgadkari@Lykteks-MacBook-Pro.local>
2024-12-16 09:55:21 +0530

    added env variables

--- firebase.js
+++ firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDCpgBfbp4AZd0l1b1uKsZXAls7wVSLsWc",
-  authDomain: "fir-auth-a5a10.firebaseapp.com",
-  projectId: "fir-auth-a5a10",
-  storageBucket: "fir-auth-a5a10.appspot.com",
-  messagingSenderId: "317193971195",
-  appId: "1:317193971195:web:ca9f08ad1b296adc2f575e",
-  measurementId: "G-PV66RB7C7Z"
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId:process.env.MESSAGINGSENDERID ,
+  appId: process.env.APPID,
+  measurementId: process.env.MEASUREMENTId
 };
 
 // Initialize Firebase
